00ff2a044befc0c302c39193919ee1171c7ce312
matty147 <matty.hana@gmail.com>
2024-04-10 00:22:18 +0200

    added some comments into todo and removed usless coments in code

--- .js/index.js
+++ .js/index.js
@@ -3,23 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const menu = document.getElementById("menu");
 
     burgerIcon.addEventListener("click", function(event) {
-        // Prevent the click event from propagating to the document
         event.stopPropagation();
-        // Toggle the visibility of the menu
         menu.style.visibility = (menu.style.visibility === "visible") ? "hidden" : "visible";
 
         if (menu.style.visibility === "visible") {
-            // Add an event listener to the document to hide the menu when clicking outside of it
             document.addEventListener("click", hideMenu);
         } else {
-            // Remove the event listener when the menu is hidden
             document.removeEventListener("click", hideMenu);
         }
     });
 
-    // Function to hide the menu
     function hideMenu(event) {
-        // Check if the click event target is outside of the menu
         if (!menu.contains(event.target) && event.target !== burgerIcon) {
             menu.style.visibility = "hidden";
         }
